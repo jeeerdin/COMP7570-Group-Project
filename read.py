71f20618a1e4e7edd594f8d6d862d55f6adc47ad
Judah Zammit <43350543+JudahZammit@users.noreply.github.com>
2021-10-18 11:33:23 -0500

    Add files via upload

--- read.py
+++ read.py
@@ -1,81 +1,54 @@
 YEAR = 2015
 DATA_PATH = "./data/edges{}/".format(YEAR)
 
-
 input_files = [DATA_PATH + "inputs{}_{}.txt".format(YEAR,i) for i in range(1,13)]
 output_files = [DATA_PATH + "outputs{}_{}.txt".format(YEAR,i) for i in range(1,13)]
-
-hash_to_hex = lambda h : hex(int(h,16))
 
 def parse_input_line(input_line):
 
   # Remove end of line
-  input_line = input_line[:-2]
+  input_line = input_line[:-1]
 
   # Split by tab
   input_line = input_line.split('\t')
 
-  line_len = len(input_line) # Remove for efficiency
-
-  # Pares from string to appropiate data type
-  parsed_line = []
-
   # Parse time
-  time = input_line.pop(0)
-  parsed_line.append(int(time))
+  input_line[0] = int(input_line[0])
 
   # Parse transaction hash
-  t_hash = input_line.pop(0)
-  parsed_line.append(hash_to_hex(t_hash))
+  input_line[1] = hex(int(input_line[1],16))
 
-  for _ in range(len(input_line)//2):
+  for i in range(2,len(input_line),2):
     # Parse the first input's transaction hash
-    input_hash = input_line.pop(0)
-    parsed_line.append(hash_to_hex(input_hash))
-  
+    input_line[i] = hex(int(input_line[i],16))
+    
     # Parse the first input's index
-    input_index = input_line.pop(0)
-    parsed_line.append(int(input_index))
-
-  assert len(input_line) == 0 # Remove for efficiency
-  assert line_len == len(parsed_line)# Remove for efficiency
-
-  return parsed_line
+    input_line[i+1] = int(input_line[i+1])
+  
+  return input_line
 
 def parse_output_line(output_line):
 
   # Remove end of line
-  output_line = output_line[:-2]
+  output_line = output_line[:-1]
 
   # Split by tab
   output_line = output_line.split('\t')
 
-  line_len = len(output_line) # Remove for efficiency
-
-  # Pares from string to appropiate data type
-  parsed_line = []
-
   # Parse time
-  time = output_line.pop(0)
-  parsed_line.append(int(time))
+  output_line[0] = int(output_line[0])
 
   # Parse transaction hash
-  t_hash = output_line.pop(0)
-  parsed_line.append(hash_to_hex(t_hash))
+  output_line[1] = hex(int(output_line[1],16))
 
-  for _ in range(len(output_line)//2):
+  for i in range(2,len(output_line),2):
     # Parse the first output's address
-    output_add = output_line.pop(0)
-    parsed_line.append(output_add)
+    output_line[i] = output_line[i]
   
     # Parse the first output's amount
-    output_amount = output_line.pop(0)
-    parsed_line.append(int(output_amount))
+    output_line[i+1] = int(output_line[i+1])
 
-  assert len(output_line) == 0 # Remove for efficiency
-  assert line_len == len(parsed_line)# Remove for efficiency
-
-  return parsed_line
+  return output_line
 
 
 def read_all_input():
@@ -94,9 +67,6 @@ def read_all_output():
 
     for output_line in file_object:
       # TODO: do something with this
-      print(parse_output_line(output_line))
+      parse_output_line(output_line)
       
     file_object.close()
-
-read_all_input()
-read_all_output()
