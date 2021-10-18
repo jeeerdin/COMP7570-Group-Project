ac32739a85feb47ea110f2f19b34ca4085da2790
Judah Zammit <43350543+JudahZammit@users.noreply.github.com>
2021-10-18 12:37:01 -0500

    Add files via upload

--- read.py
+++ read.py
@@ -1,4 +1,6 @@
-YEAR = 2015
+import threading
+
+YEAR = 2010
 DATA_PATH = "./data/edges{}/".format(YEAR)
 
 input_files = [DATA_PATH + "inputs{}_{}.txt".format(YEAR,i) for i in range(1,13)]
@@ -51,22 +53,52 @@ def parse_output_line(output_line):
   return output_line
 
 
-def read_all_input():
-  for i in range(12):
-    file_object = open(input_files[0], "r")
+# Stores a transaction as Hash : (time,inputs,output)
+transactions = {}
+
+def read_input_file(i):
+    file_object = open(input_files[i], "r")
 
     for input_line in file_object:
-      # TODO: do something with this
-      parse_input_line(input_line)
+      l = parse_input_line(input_line)
+      time = l[0]
+      hashh = l[1]
+      inputs = l[2:]
+      pretty_inputs = []
+      for i in range(0,len(inputs),2):
+          pretty_inputs.append({'Transaction Hash':inputs[i],
+                                'Output Index':inputs[i+1],
+                                'Address':None,
+                                'Amount':None})
+      transactions[hashh] = {'Time':time,'Inputs':pretty_inputs,'Outputs':None}
 
     file_object.close()
+
+def read_output_file(i):
+    file_object = open(output_files[i], "r")
+
+    for output_line in file_object:
+      l = parse_output_line(output_line)
+      hashh = l[1]
+      outputs = l[2:]
+      pretty_outputs = []
+      for i in range(0,len(outputs),2):
+          pretty_outputs.append({'Address':outputs[i],
+                                'Amount':outputs[i+1]})
+      transactions[hashh]['Outputs'] = pretty_outputs
+
+    file_object.close()
+
+
+def read_all_input():
+  for i in range(12):
+    read_input_file(i)  
 
 def read_all_output():
   for i in range(12):
-    file_object = open(output_files[0], "r")
+    read_output_file(i)
 
-    for output_line in file_object:
-      # TODO: do something with this
-      parse_output_line(output_line)
-      
-    file_object.close()
+read_all_input()
+read_all_output()
+
+# TODO: in the transactions dictionary, the address and amount of the inputs are None. This needs to be filled up I think this is "linking the inputs and outputs
