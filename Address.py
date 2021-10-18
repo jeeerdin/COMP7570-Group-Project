1da8271c7b7b3dc41093c3e7b68e27b4bb796e8c
Jeeerdin <kashtonjordan@gmail.com>
2021-10-18 15:33:31 -0500

    Create Address.py

---
+++ Address.py
@@ -0,0 +1,8 @@
+class Address:
+    def __init__(add_hash):
+        self.add_hash = add_hash
+        spending_transactions = []
+        earning_transactions = []
+        coins_connected = []
+
+    
