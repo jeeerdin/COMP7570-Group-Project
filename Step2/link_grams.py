fe981e9ddcb287927898fabee44168a659ce5dbe
Judah <zammitj3@myumanitoba.ca>
2021-11-18 16:17:06 -0600

    Actually add multi-threading

--- Step2/link_grams.py
+++ Step2/link_grams.py
@@ -1,51 +1,64 @@
 import numpy as np
 import os
 from scipy import sparse as sp
+import threading
 
-# TODO: Multi-thread code
-# TODO: Cap at 1000
+THREADS = 8
 
-
-days = os.listdir('../data/grams/parsed/')
+prices = np.load('../data/grams/parsed/judah.npy')
 tx_to_addr = sp.load_npz('../data/daily_graphs/months_1_to_12_btc_transactions_tx_to_addr.npz').tocsc()
-dirty = {}
 
-[]
-for day in days:
-    # Load the grams data for that day
-    
-    prices = np.load('../data/grams/parsed/'+day) 
+# Remove zero prices because they are not very unique
+prices = prices[prices[:,0] != 0]
 
-    # Remove zero prices because they are not very unique
-    prices = prices[prices != 0]
+counts = prices[:,1]
+prices = prices[:,0]
 
-    # Remove non unique valuse
-    prices,counts = np.unique(prices,return_counts = True)
+l = prices.shape[0]
+indices = []
+for i in range(THREADS):
+    indices.append((i*l//THREADS,(i+1)*l//THREADS))
 
-    for i in range(1000,len(prices)):
+all_ = []
 
-        p = prices[i]
-        count = counts[i]
+def search(start,end):
+        global all_
+#
+        i = start
+#
+        dirty = []
+        while len(dirty) <= 122000//THREADS//24//12 and i < end:
+#
+            p = prices[i]
+            count = counts[i]
+#
+            # Find all entries in the tx_to_addr matrix that matches that price
+            find = tx_to_addr == p
+            idx = find.indices
+            num_found = len(idx)
+#
+            # For each transaction found
+            for tId in idx:
+#
+                # Print it out
+#                print("tId = {} is a darknet transaction with confidence {}".format(tId,count*(num_found**-1)))
+#            
+                dirty.append((tId,count*(num_found**-1)))
+#
+            i+=1
+        all_ = all_ + dirty
 
-        # Find all entries in the tx_to_addr matrix that matches that price
-        find = tx_to_addr == p
-        idx = find.indices
-        num_found = len(idx)
 
-        # For each transaction found
-        for tId in idx:
+threads =[]
+for idx in indices:
+    t = threading.Thread(target = search, args = (idx[0],idx[1]))
+    t.start()
+    threads.append(t)
 
-            # Print it out
-            print("tId = {} is a darknet transaction with confidence {}".format(tId,count*(num_found**-1)))
-            
-            # Append it to a list of all darknet transactions
-            try:
-                dirty[tId] += count*(num_found**-1)
-            except:
-                dirty[tId] = count*(num_found**-1)
+for t in threads:
+    t.join()
 
-
-print(dirty)
+print(all_)
 
 # Save all the darknet transaction ID to file
-#np.save('../data/grams/linked/bad_tId.npy',np.array(dirty))
+np.save('../data/grams/linked/judah_bad_tId.npy',np.array(all_))
