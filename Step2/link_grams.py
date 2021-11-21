26689ea28f8bb638c6987594851562faeb8cb612
Abram Kremer <abramkremer1@gmail.com>
2021-11-20 22:05:12 -0600

    Cleanup and comments

--- Step2/link_grams.py
+++ Step2/link_grams.py
@@ -1,5 +1,4 @@
 import numpy as np
-import os
 from scipy import sparse as sp
 import threading
 
@@ -11,9 +10,12 @@ tx_to_addr = sp.load_npz('../data/daily_graphs/months_1_to_12_btc_transactions_t
 # Remove zero prices because they are not very unique
 prices = prices[prices[:,0] != 0]
 
+# the number of darknet transactions with this amount
 counts = prices[:,1]
+# the amount
 prices = prices[:,0]
 
+# dividing the prices data into equal sections for each thread
 l = prices.shape[0]
 indices = []
 for i in range(THREADS):
@@ -21,14 +23,19 @@ for i in range(THREADS):
 
 all_ = []
 
+# links darknet amounts to addresses from indices start to end
 def search(start,end):
         global all_
 #
         i = start
 #
+        # loops through the indices of the prices data until it reaches the end
+        # will also stop if the number of linked addresses hits the goal of 122,000
+        # This number was calculated by 1000 * 365 = 365,000 / 3 =  ~122,000
         dirty = []
         while len(dirty) <= 122000//THREADS and i < end:
 #
+            # gets the amount at this index and also the unmber of times this amount appeared in darknet data
             p = prices[i]
             count = counts[i]
 #
@@ -40,16 +47,15 @@ def search(start,end):
             # For each transaction found
             for tId in idx:
 #
-                # Print it out
-#                print("tId = {} is a darknet transaction with confidence {}".format(tId,count*(num_found**-1)))
-#               
+                # calculates a confidence to assign to that addres being darknet
+                # only addresses with a confidence >= 50% are included in the list of suspected darknet addresses
                 if count*(num_found**-1) >= .5:
                     dirty.append((tId,count*(num_found**-1)))
 #
             i+=1
         all_ = all_ + dirty
 
-
+# for each thread they use their own equal sized disjoint sets of indices to run through
 threads =[]
 for idx in indices:
     t = threading.Thread(target = search, args = (idx[0],idx[1]))
@@ -58,8 +64,6 @@ for idx in indices:
 
 for t in threads:
     t.join()
-
-print(all_)
 
 # Save all the darknet transaction ID to file
 np.save('../data/grams/linked/judah_bad_tId.npy',np.array(all_))
