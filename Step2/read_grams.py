019e034433676147f81ffbdfd4c59793ea8a5b98
Abram Kremer <abramkremer1@gmail.com>
2021-11-20 21:53:05 -0600

    Cleanup and comments

--- Step2/read_grams.py
+++ Step2/read_grams.py
@@ -1,22 +1,27 @@
 import numpy as np
 import os
 import pandas as pd
-import json
 
+# getting list of grams data folders
 folders = os.listdir('../data/grams/raw/')
 
 csv = {}
 
+# taking the files in each folder and placing them in dictionary
 for folder in folders:
     csv[folder] = os.listdir('../data/grams/raw/'+folder)
 
 prices = {}
 
 for folder in folders:
+
+    # each file represents a different darknet market for that day
     markets = csv[folder]
+
     tx = []
     for m in markets:
         try:
+            # appending all the transaction amounts from each market to the list tx
             x = pd.read_csv('../data/grams/raw/{}/{}'.format(folder,m),on_bad_lines='warn')['price'].to_numpy()
             tx.append(x)
         except Exception:
@@ -24,14 +29,18 @@ for folder in folders:
 
     if len(tx) == 0:
         pass
+
     price = np.concatenate(tx)
     
+    # formatting the month and day
     month = str(int(folder[5:7]))
     day = str(int(folder[8:10]))
     mmdd = month + '_' + day
 
+    # the prices dictionary at that date will point to the list of tx amounts
     prices[mmdd] = price
 
+# fomatting the data and then storing it in a file for each day
 for mmdd in prices:
     arr = prices[mmdd]
     arr = arr*1e8
