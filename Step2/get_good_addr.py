89209af801bce3135550d1d2ddf1dcca912878e9
Abram Kremer <abramkremer1@gmail.com>
2021-12-15 20:39:13 -0600

    Added comments

--- Step2/get_good_addr.py
+++ Step2/get_good_addr.py
@@ -10,16 +10,18 @@ for i in range(1,12):
     days_per_month.append(monthrange(2015, i)[1])
 days_per_month = np.cumsum(days_per_month)
 
+# iterates through each month and day
 for month in range(1,13):
   for day in range(1,monthrange(2015,month)[1]+1):
 
+    # this is calculated like being the i'th day of the year, for example Feb. 9th is the 40th day of the year
     raw_day = days_per_month[month - 1] + day
 
     tx_to_addr = sp.load_npz('../data/daily_graphs/{}_{}_tx_to_addr.npz'.format(month,day)).astype('uint64').tocsr()
     addr_to_tx = sp.load_npz('../data/daily_graphs/{}_{}_addr_to_tx.npz'.format(month,day)).astype('uint64').tocsc()
 
+    # load in this day's list of darknet addresses
     num_bad = np.load('../data/grams/linked/bad_addr/{}.npy'.format(raw_day)).shape[0]
-    
     
     i = tx_to_addr.indices
     np.random.shuffle(i)
