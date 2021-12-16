4084c6784fb41d26c37727a41a00ff311ace2e12
Abram Kremer <abramkremer1@gmail.com>
2021-12-15 20:54:53 -0600

    Making comments

--- Step2/make_X_y.py
+++ Step2/make_X_y.py
@@ -8,10 +8,14 @@ for day in range(1,365):
     good_addr = np.load('../data/grams/linked/good_addr/{}.npy'.format(day))
     bad_addr = np.load('../data/grams/linked/bad_addr/{}.npy'.format(day))
 
+    # getting the list of features associated with our lists of good and bad addesses
     good_features = features[good_addr]
     bad_features = features[bad_addr]
 
+    # concatenating the features lists for both good and bad into X
     X = np.concatenate([good_features,bad_features],axis = 0)
+    
+    # labelling each good address with a 0 and each darknet with a 1
     y = np.concatenate([np.zeros_like(good_addr),np.ones_like(bad_addr)],axis = 0)
 
     np.save('../data/grams/X/{}.npy'.format(day),X)
